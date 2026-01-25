cc12b1f2b2a753e27485b079dcba237c361e6f1c
Matheus <matheusmavieira@gmail.com>
2026-01-25 15:23:44 -0300

    Setup knexjs for database

---
+++ knexfile.js
@@ -0,0 +1,41 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database.sqlite'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './migrations'
+    }
+  },
+
+  staging: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database.sqlite'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './migrations'
+    }
+  },
+
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database.sqlite'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './migrations'
+    }
+  }
+
+};
---
+++ migrations/20260125181356_create_users_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('user', function(table) {
+    table.string('id').primary();
+    table.string('username').unique().notNullable();
+    table.string('password').notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('user');
+};
